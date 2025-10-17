daac5d17b1945024a44345c523a1507526b374c8
Nishka Jain <nishkaj@uci.edu>
2025-10-17 12:18:42 -0700

    starter

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <main className="flex flex-col items-center gap-8 py-16 max-w-[1280px] mx-auto">
+      <h1 className="text-4xl font-bold">Hello React & Tailwind!</h1>
+      <div className="flex flex-row items-center gap-6">
+      </div>
+    </main>
+  );
+}
+
+export default App;
