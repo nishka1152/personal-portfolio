6d82e11d653eb196be2a0d0ee5dc7dfadcc23920
Nishka Jain <nishkaj@uci.edu>
2025-10-20 01:15:46 -0700

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
+import AuroraBackgroundDemo from './components/background'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
+        <AuroraBackgroundDemo/>
         <Hero />
         <About />
         <Projects />
